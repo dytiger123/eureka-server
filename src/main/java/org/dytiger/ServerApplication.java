d215d8d6615d9962e3451fab0f8669617241ff87
root <root@myharbor.com>
2020-03-11 23:00:18 +0800

    配置修改

--- src/main/java/org/dytiger/ServerApplication.java
+++ src/main/java/org/dytiger/ServerApplication.java
@@ -4,14 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
-public class ServerApplication{
- 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
- 
+public class ServerApplication extends SpringBootServletInitializer {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ServerApplication.class);
+    }
+	
+	 public static void main(String[] args) {
+		 SpringApplication.run(ServerApplication.class, args);
+	 }
+	
 }
