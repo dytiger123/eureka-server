b5bf3eb6544dc1402af2d0969fd77fbcaea3551d
root <root@myharbor.com>
2020-03-11 21:49:03 +0800

    11

---
+++ src/main/java/org/dytiger/ServerApplication.java
@@ -0,0 +1,17 @@
+package org.dytiger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+public class ServerApplication{
+ 
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+ 
+}
